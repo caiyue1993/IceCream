22d1cd5d4c1951fc74142b316ea1f88d92c0e4a4
Yue Cai <yuecai.nju@gmail.com>
2017-11-08 18:23:18 +0800

    update engine

--- SyncEngine.swift
+++ SyncEngine.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import RealmSwift
+
+public final class SyncEngine {
+    
+    let objectType: Object
+    
+    private func subscribeToLocalDatabase() {
+        let dogs = Cream.shared.realm.objects(<#T##type: Element.Type##Element.Type#>)
+    }
+}
