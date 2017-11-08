31ed6ac333b5baf6014ce0cfb5c635c6be081175
Yue Cai <yuecai.nju@gmail.com>
2017-11-08 11:24:23 +0800

    clear up old stuff

--- IceCream/Classes/Cream.swift
+++ IceCream/Classes/Cream.swift
@@ -1,36 +1,42 @@
-
-
 import Foundation
 import RealmSwift
 
-final class Cream {
+/// Cream is the Alfred of Realm.
+/// You do insert/update/delete with Cream instead of manipulating Realm itself.
+
+public final class Cream {
     
+    /// The original realm that Cream dances with.
     private let realm: Realm
-    static let shared = Cream()
     
-    init(realm: Realm?) {
-        if let r = realm { self.realm = r }
-        else { self.realm = try! Realm() }
-    }
+    // MARK: - Singleton
+    public static let shared = Cream()
     
-    public convenience init() {
-        self.init(realm: nil)
-    }
-    
-    private func insertOrUpdate<T: Object>(object: T) throws {
-        guard let primaryKey = T.primaryKey() else {
-            fatalError("Can not execute insertOrUpdate when no primaryKey in \(T.description())")
+    // MARK: - Initializer
+    init(realm: Realm? = nil) {
+        if let r = realm {
+            self.realm = r
+        } else {
+            self.realm = try! Realm()
         }
-        
-        guard let primaryKeyValue = object.value(forKey: primaryKey) else {
-            fatalError("Can not find the given primaryKey value of \(T.description())")
-        }
+    }
+}
+
+/// Specific manipulation of Realm
+public extension Cream {
+    
+    // MARK: - Insert or Update
+    func insertOrUpdate<T: Object>(object: T) throws {
+        guard let primaryKey = T.primaryKey() else { fatalError("Can not execute insertOrUpdate when no primaryKey in \(T.description())") }
+        guard let primaryKeyValue = object.value(forKey: primaryKey) else { fatalError("Can not find the given primaryKey value of \(T.description())") }
         
         if let _ = realm.object(ofType: T.self, forPrimaryKey: primaryKeyValue) {
+            /// Update
             realm.beginWrite()
             realm.add(object, update: true)
             try realm.commitWrite()
         } else {
+            /// Insert new
             realm.beginWrite()
             realm.add(object)
             try realm.commitWrite()
