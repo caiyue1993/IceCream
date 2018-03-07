6587e4bdd4e954f05600dacfbcfe156db1fce813
Andrew Eades <andy@intrepidfrog.com>
2018-03-07 10:19:27 +0000

    Adding new example to test code with.

---
+++ Examples/IceCreamExample2/IceCreamExample2/Sources/Dog.swift
@@ -0,0 +1,34 @@
+//
+//  Dog.swift
+//  IceCream_Example
+//
+//  Created by 蔡越 on 23/10/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import IceCream
+import CloudKit
+
+class Dog: Object {
+    @objc dynamic var id = NSUUID().uuidString
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+    @objc dynamic var isDeleted = false
+
+    static let AVATAR_KEY = "avatar"
+    @objc dynamic var avatar: CreamAsset?
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+extension Dog: CKRecordConvertible {
+    
+}
+
+extension Dog: CKRecordRecoverable {
+    typealias O = Dog
+}
