adeae5e69e14967a5f3fd2852403f3b00373ec08
David <bitomule@gmail.com>
2018-04-30 18:34:51 +0200

    Make realm inyectable

--- IceCream/Classes/RealmQueuer.swift
+++ IceCream/Classes/RealmQueuer.swift
@@ -1,50 +1,51 @@
 import Foundation
 import RealmSwift
 
-class RealmQueuer {    
+class RealmQueuer {
     let delay: TimeInterval
     var operations: [(Realm) -> Void] = [(Realm) -> Void]()
     fileprivate let throttler: Throttler
-    
+    private let realm = try! Realm()
+
     init(delay: TimeInterval = 0.5) {
         self.delay = delay
         throttler = Throttler(seconds: delay)
     }
-    
+
     func appendOperation(_ operation: @escaping (Realm) -> Void) {
         operations.append(operation)
     }
-    
-    func execute(notificationToken: NotificationToken? = nil) {
+
+    func execute(customRealm: Realm? = nil, notificationToken: NotificationToken? = nil) {
         throttler.throttle { [weak self] in
             guard let `self` = self else { return }
+            let definedRealm = customRealm ?? `self`.realm
             /// If your model class includes a primary key, you can have Realm intelligently update or add objects based off of their primary key values using Realm().add(_:update:).
             /// https://realm.io/docs/swift/latest/#objects-with-primary-keys
-            let realm = try! Realm()
-            realm.beginWrite()
+            definedRealm.beginWrite()
             for op in `self`.operations {
-                op(realm)
+                op(definedRealm)
             }
             if let token = notificationToken {
-                try! realm.commitWrite(withoutNotifying: [token])
+                try! definedRealm.commitWrite(withoutNotifying: [token])
             } else {
-                try! realm.commitWrite()
+                try! definedRealm.commitWrite()
             }
         }
     }
 }
 
 private class Throttler {
-    private let queue: DispatchQueue = DispatchQueue.global(qos: .background)
-    
+    private let queue: DispatchQueue = DispatchQueue.main
+
     private var job: DispatchWorkItem = DispatchWorkItem(block: {})
     private var previousRun: Date = Date.distantPast
     private var maxInterval: TimeInterval
-    
+
     init(seconds: TimeInterval) {
         self.maxInterval = seconds
     }
-    
+
     func throttle(block: @escaping () -> ()) {
         job.cancel()
         job = DispatchWorkItem(){ [weak self] in
