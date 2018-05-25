ffa0a3d0077b908dcf54a26650b55b8dd9277f21
Yue Cai <yuecai.nju@gmail.com>
2018-05-26 01:35:34 +0800

    updated the demo usage

--- Example/IceCream_Example/AppDelegate.swift
+++ Example/IceCream_Example/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        syncEngine = SyncEngine(sources: [
+        syncEngine = SyncEngine(syncObjects: [
             SyncObject<Dog>(),
             SyncObject<Cat>()
             ])
