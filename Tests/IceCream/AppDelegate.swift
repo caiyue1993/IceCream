564a90edb29c17313fe1b75eab2c27901767f3d2
David <bitomule@gmail.com>
2018-08-08 23:45:45 +0200

    Add tests project

---
+++ Tests/IceCream/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  IceCream
+//
+//  Created by David Collado on 8/8/18.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Tests/Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'IceCreamTests' do
+  pod 'IceCream', :path => '../'
+end
