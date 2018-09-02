a9844f3922262ce1002de534df97e08e85ecac9b
Yue Cai <yuecai.nju@gmail.com>
2018-09-02 15:54:35 +0800

    add watchOS target

---
+++ IceCream-watchOS/IceCream_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  IceCream_watchOS.h
+//  IceCream-watchOS
+//
+//  Created by 蔡越 on 2018/9/2.
+//  Copyright © 2018 蔡越. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for IceCream_watchOS.
+FOUNDATION_EXPORT double IceCream_watchOSVersionNumber;
+
+//! Project version string for IceCream_watchOS.
+FOUNDATION_EXPORT const unsigned char IceCream_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IceCream_watchOS/PublicHeader.h>
+
+
