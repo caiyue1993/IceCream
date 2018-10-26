4e57ee2cfc815f1363cd98507b82b1379e2e9a02
Julian <justdotjs@icloud.com>
2018-10-26 19:13:48 +0800

    Resolve an issue

--- IceCream/Classes/CKRecordRecoverable.swift
+++ IceCream/Classes/CKRecordRecoverable.swift
@@ -21,32 +21,32 @@ extension CKRecordRecoverable where Self: Object {
             if prop.isArray {
                 switch prop.type {
                 case .int:
-                    let value = record.value(forKey: prop.name) as? [Int]
+                    guard let value = record.value(forKey: prop.name) as? [Int] else { break }
                     let list = List<Int>()
                     list.append(objectsIn: value)
                     recordValue = list
                 case .string:
-                    let value = record.value(forKey: prop.name) as? [String]
+                    guard let value = record.value(forKey: prop.name) as? [String] else { break }
                     let list = List<String>()
                     list.append(objectsIn: value)
                     recordValue = list
                 case .bool:
-                    let value = record.value(forKey: prop.name) as? [Bool]
+                    guard let value = record.value(forKey: prop.name) as? [Bool] else { break }
                     let list = List<Bool>()
                     list.append(objectsIn: value)
                     recordValue = list
                 case .float:
-                    let value = record.value(forKey: prop.name) as? [Float]
+                    guard let value = record.value(forKey: prop.name) as? [Float] else { break }
                     let list = List<Float>()
                     list.append(objectsIn: value)
                     recordValue = list
                 case .double:
-                    let value = record.value(forKey: prop.name) as? [Double]
+                    guard let value = record.value(forKey: prop.name) as? [Double] else { break }
                     let list = List<Double>()
                     list.append(objectsIn: value)
                     recordValue = list
                 case .data:
-                    let value = record.value(forKey: prop.name) as? [Data]
+                    guard let value = record.value(forKey: prop.name) as? [Data] else { break }
                     let list = List<Data>()
                     list.append(objectsIn: value)
                     recordValue = list
