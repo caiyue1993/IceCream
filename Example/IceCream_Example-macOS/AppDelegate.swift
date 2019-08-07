ad96d37b76eede24538ea6f6962737a359998c05
Sol Cai <sol.cai@liulishuo.com>
2019-08-07 18:47:05 +0800

    init macos example target

---
+++ Example/IceCream_Example-macOS/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  IceCream_Example-macOS
+//
+//  Created by caiyue on 2019/8/7.
+//  Copyright © 2019 蔡越. All rights reserved.
+//
+
+import Cocoa
+import IceCream
+import CloudKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+    var syncEngine: SyncEngine?
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func application(_ application: NSApplication, didReceiveRemoteNotification userInfo: [String : Any]) {
+        if let notification = CKNotification(fromRemoteNotificationDictionary: userInfo), let subscriptionID = notification.subscriptionID, IceCreamSubscription.allIDs.contains(subscriptionID) {
+            NotificationCenter.default.post(name: Notifications.cloudKitDataDidChangeRemotely.name, object: nil, userInfo: userInfo)
+        }
+    }
+
+
+}
+
