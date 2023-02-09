6b31ae225fc88c29a5c73b905aa3f553184e6f3a
leoz <leoz@yahoo.com>
2023-02-09 09:10:19 -0500

    Use RealmSwift 10.35.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/realm/realm-swift",
-            from: "10.34.1"
+            from: "10.35.0"
         )
     ],
     targets: [
