e1953cfe696c6381d9fb5f91f602b8f1507a7cd2
Yue Cai <yuecai.nju@gmail.com>
2025-06-13 19:45:13 +0800

    for the ios26 baby

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/realm/realm-cocoa", 
-            from: "4.1.1"
+            url: "https://github.com/caiyue1993/realm-cocoa",
+            .branch("feature/ios26")
         )
     ],
     targets: [
